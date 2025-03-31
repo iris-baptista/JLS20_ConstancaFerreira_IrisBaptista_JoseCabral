27319edd8d0a828028eb1329144dbed34360a80b
ConchaCF <118653385+ConchaCF@users.noreply.github.com>
2025-03-31 13:39:53 +0100

    Codigo Ze (comentado por Concha sorry XD)

--- src/Stone.scala
+++ src/Stone.scala
@@ -1,3 +1,5 @@
+import scala.util.Random
+
 object Stone extends Enumeration{
   type Board = List[List[Stone]]
   type Coord2D = (Int,Int)
@@ -6,9 +8,19 @@ object Stone extends Enumeration{
   val Black,White,Empty = Value
 
   //T1
-//  def randomMove(lstOpenCoords:List[Coord2D], rand:MyRandom):(Coord2D,MyRandom){
-//
-//  }
+  //gerar uma coordenada aleatória
+  //válida para a próxima jogada a partir da lista de posições livres fornecidas
+  def randomMove(lstOpenCoords: List[Coord2D], rand:MyRandom):(Coord2D, MyRandom) = {
+    val (index, newRand) = rand.tirarRandao(lstOpenCoords.size) //isto gera de 0-size ou de 0-(size-1)?
+    (lstOpenCoords(index), newRand) //devolve uma posicao valida como vem da lista!
+  }
+
+  class MyRandom {
+    def tirarRandao(max: Int): (Int, MyRandom) = {
+      val randNum = Random.nextInt(max) // Gera um randu de 0 a (max - 1), q sera o size()
+      (randNum, this)
+    }
+  }
 
   //T2
 //  def play(board:Board, player:Stone, coord:Coord2D, lstOpenCoords:List[Coord2D]):(Option[Board], List[Coord2D]){
@@ -21,9 +33,9 @@ object Stone extends Enumeration{
 //  }
 
   //T4
-  def printBoard(board:Board): Unit = {
+  /*def printBoard(board:Board): Unit = {
 
-  }
+  } */
 
   def main(args: Array[String]): Unit = {
     println("amongus")
