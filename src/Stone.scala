3e25fc34677e13ebc267da1ce0f37a961d551d78
ConchaCF <118653385+ConchaCF@users.noreply.github.com>
2025-03-31 14:36:27 +0100

    Segunda ronda de testes

--- src/Stone.scala
+++ src/Stone.scala
@@ -1,4 +1,5 @@
 import scala.annotation.tailrec
+import scala.io.StdIn.readLine
 
 object Stone extends Enumeration{
   type Stone = Value
@@ -99,20 +100,69 @@ object Stone extends Enumeration{
 
   }*/
 
+  def displayM(m: Board): Unit = {
+    m match {
+      case Nil => return
+      case x :: xs => {
+        displayLine(x)
+        println()
+        displayM(xs)
+      }
+    }
+
+    println()
+
+    def displayLine(l: List[Stone]): Unit = {
+      l match {
+        case Nil => return
+        case x :: xs => {
+          print(x)
+          print(" ")
+          displayLine(xs)
+        }
+      }
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     //testes
-    val board = List(List(Stone.Empty, Stone.Empty), List(Stone.Empty, Stone.Empty)) //cria um 2x2
+    val board = List(List(Stone.Empty, Stone.Empty, Stone.Empty, Stone.Empty, Stone.Empty),
+      List(Stone.Empty, Stone.Empty, Stone.Empty, Stone.Empty, Stone.Empty),
+      List(Stone.Empty, Stone.Empty, Stone.Empty, Stone.Empty, Stone.Empty),
+      List(Stone.Empty, Stone.Empty, Stone.Empty, Stone.Empty, Stone.Empty),
+      List(Stone.Empty, Stone.Empty, Stone.Empty, Stone.Empty, Stone.Empty)) //cria um 5x5
     val player = Stone.Black
-    val cNova = (0, 1)
-    val cLivres = List((0, 1), (2, 1), (0, 0))
+    //val cNova= (0,1)
+    val cLivres = List((0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (2, 0),
+      (2, 1), (2, 2), (2, 3), (2, 4), (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4))
 
-    val (newBoard, newLivres)= Stone.play(board, player, cNova, cLivres)
+    /*val (newBoard, newLivres)= Stone.play(board, player, cNova, cLivres)
     println(newLivres)
-    println(newBoard.getOrElse(None))
+    println(newBoard.getOrElse(None))*/
 
-    val (nextBoard, nextLivres) = Stone.play(newBoard.get, player, (4, 5), newLivres)
-    println(newLivres)
-    println(nextBoard.getOrElse(None))
+    val (nextB1, nextL1) = Stone.play(board, player, (0, 1), cLivres)
+    //println(nextB1.getOrElse(None))
+    displayM(nextB1.get)
+
+    val (nextB2, nextL2) = Stone.play(nextB1.get, player, (0, 1), nextL1)
+    println(nextB2.getOrElse(None))
+    //displayM(nextB2.get)
+
+    val (nextB3, nextL3) = Stone.play(nextB1.get, player, (2, 2), nextL1)
+    //println(nextB3.getOrElse(None))
+    displayM(nextB3.get)
+
+    val (nextB4, nextL4) = Stone.play(nextB3.get, player, (4, 3), nextL3)
+    //println(nextB4.getOrElse(None))
+    displayM(nextB4.get)
+
+    val (nextB5, nextL5) = Stone.play(nextB4.get, player, (1, 2), nextL4)
+    //println(nextB5.getOrElse(None))
+    displayM(nextB5.get)
+
+    val (nextB6, nextL6) = Stone.play(nextB5.get, player, (7, 6), nextL5)
+    println(nextB6.getOrElse(None))
+    //displayM(nextB6.get)
   }
 
 }
