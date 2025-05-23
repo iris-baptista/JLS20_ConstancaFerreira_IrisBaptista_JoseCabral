af92a3063ca8fc581848c368e5c6a9cab806ae28
iris <alex2005iris@gmail.com>
2025-05-23 13:09:05 +0100

    gui atualizada (not finished)

---
+++ src/main/scala/GUI.scala
@@ -0,0 +1,21 @@
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.{Parent, Scene}
+import javafx.stage.Stage
+
+class GUI extends Application {
+  override def start(primaryStage: Stage) = {
+    primaryStage.setTitle("Go! - The Game")
+    val fxmlLoader = new FXMLLoader(getClass.getResource("mainMenu.fxml"))
+    val mainViewRoot: Parent = fxmlLoader.load()
+    val scene = new Scene(mainViewRoot)
+    primaryStage.setScene(scene)
+    primaryStage.show()
+  }
+}
+
+object FxApp {
+  def main(args: Array[String]) = {
+    Application.launch(classOf[GUI], args: _*)
+  }
+}
--- src/main/scala/SceneController.scala
+++ src/main/scala/SceneController.scala
@@ -1,5 +1,82 @@
-class SceneController {
-  def switchToGame() = {
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.{Parent, Scene}
+import javafx.scene.control.{Button, Label}
+import javafx.scene.layout.AnchorPane
+//import javafx.scene.shape.{Rectangle}
+import javafx.fxml.FXMLLoader
+import javafx.scene.layout.GridPane
+import javafx.scene.paint.Color
+import javafx.stage.Stage
 
+
+class SceneController {
+
+  @FXML
+  private var startButton: Button = _
+  @FXML
+  private var exitButton1: Button = _
+  @FXML
+  private var exitButton2: Button = _
+  @FXML
+  private var gameTitle: Label = _
+  @FXML
+  private var randomMove: Button = _
+  //private var state: TextFlow = _
+  @FXML
+  private var board: GridPane = _
+
+  @FXML
+  private var label1: AnchorPane = _
+
+  @FXML
+  private var label2: AnchorPane = _
+
+
+  // Main Menu!
+  def changeColor() = {
+    if(gameTitle.getTextFill.equals(Color.BLACK)){
+      gameTitle.setTextFill(Color.WHITE)
+    } else if(gameTitle.getTextFill.equals(Color.WHITE)){
+      gameTitle.setTextFill(Color.BLACK)
+    }
+  }
+
+  def switchToGame() = {
+    val loader = new FXMLLoader(getClass.getResource("gameBoard.fxml"))
+    val root: Parent = loader.load()
+    val stage = startButton.getScene.getWindow.asInstanceOf[Stage]
+    stage.setScene(new Scene(root))
+  }
+
+  def exitMenu(): Unit = {
+    val stage = label1.getScene.getWindow
+    stage.hide()
+    println("Shutting down...")
+  }
+
+//  @FXML
+//  private var rect00: Rectangle = _
+//  private var rect01: Rectangle = _
+//  private var rect02: Rectangle = _
+//  private var rect03: Rectangle = _
+//  private var rect04: Rectangle = _
+//  private var rect10: Rectangle = _
+//  private var rect11: Rectangle = _
+//  private var rect12: Rectangle = _
+//  private var rect13: Rectangle = _
+//  private var rect14: Rectangle = _
+//  private var rect20: Rectangle = _
+
+  // gameBoard!
+  def randomPlay(): Unit = {
+    //GameEngine.playRandomly()
+    //println("Jogada aleatória!") TEXTFLOW
+  }
+
+  def exitGame(): Unit = {
+    val stage = label2.getScene.getWindow
+    stage.hide()
+    println("Shutting down...")
   }
 }
